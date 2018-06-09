4fba5d8954823da7500223ee9a1805e599d3eba3
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2018-06-08 23:21:11 -0300

    Agrego botón de back a la pantalla re resolución de ecuaciones

--- AppMobile/app/src/main/java/ar/com/profebot/activities/SolveEquationActivity.java
+++ AppMobile/app/src/main/java/ar/com/profebot/activities/SolveEquationActivity.java
@@ -1,5 +1,6 @@
 package ar.com.profebot.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
@@ -13,12 +14,20 @@ public class SolveEquationActivity extends GlobalActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.solve_equation_layout);
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_id);
         setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         super.onCreate(savedInstanceState);
 
         ((TextView) findViewById(R.id.equation_drawn_id)).setText(ExpressionsManager.getEquationDrawn());
         ((TextView) findViewById(R.id.equation_generated_id)).setText(ExpressionsManager.getTreeOfExpression().toExpression());
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        startActivity(new Intent(this, EnterEquationOptionsActivity.class));
+        return true;
     }
 }
