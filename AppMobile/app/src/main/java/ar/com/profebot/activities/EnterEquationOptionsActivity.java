7d24882739532dcf393f65c72c5bfdce244e4125
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2018-07-10 03:56:59 -0300

    Refactorizo pantalla de selección de opción de ingreso de ecuación + cambio la UI

--- AppMobile/app/src/main/java/ar/com/profebot/activities/EnterEquationOptionsActivity.java
+++ AppMobile/app/src/main/java/ar/com/profebot/activities/EnterEquationOptionsActivity.java
@@ -1,14 +1,15 @@
 package ar.com.profebot.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import com.profebot.activities.R;
-
-import ar.com.profebot.service.ExpressionsManager;
 
 public class EnterEquationOptionsActivity extends GlobalActivity {
 
@@ -22,29 +23,78 @@ public class EnterEquationOptionsActivity extends GlobalActivity {
 
         super.onCreate(savedInstanceState);
 
-        Button option;
+        ImageView handdrawEquationPicture = (ImageView) findViewById(R.id.handdraw_equation_picture_id);
+        ImageView photoEquationPicture = (ImageView) findViewById(R.id.photo_equation_picture_id);
+        RadioButton handdrawEquationOption = (RadioButton) findViewById(R.id.handdraw_equation_id);
+        RadioButton photoEquationOption = (RadioButton) findViewById(R.id.photo_equation_id);
+        Button startResolution = (Button) findViewById(R.id.start_resolution_id);
 
-        option = (Button) findViewById(R.id.handdraw_equation_id);
-        option.setOnClickListener(new View.OnClickListener() {
+        // When i touch over images
+
+        handdrawEquationPicture.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View button) {
-                Intent intent = new Intent(button.getContext(), EnterEquationHandDrawActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                handdrawEquationPicture.setImageResource(R.drawable.handdraw_equation_selected);
+                photoEquationPicture.setImageResource(R.drawable.photo_equation);
+                handdrawEquationOption.setChecked(true);
+                photoEquationOption.setChecked(false);
+                activateStartResolutionButton();
             }
         });
 
-        option = (Button) findViewById(R.id.photo_equation_id);
-        option.setOnClickListener(new View.OnClickListener() {
+        photoEquationPicture.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View button) {
-
-                Intent intent = new Intent(button.getContext(), CarmeraActivity.class);
-                //String latex = "\\frac { 3 + 2 ^ { 4 } } { 3 x } = 5";
-                //ExpressionsManager.setEquationPhoto(latex);
-                //Intent intent = new Intent(button.getContext(), SolveEquationActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                photoEquationPicture.setImageResource(R.drawable.photo_equation_selected);
+                handdrawEquationPicture.setImageResource(R.drawable.handdraw_equation);
+                photoEquationOption.setChecked(true);
+                handdrawEquationOption.setChecked(false);
+                activateStartResolutionButton();
             }
         });
+
+        // When i pick radio buttons
+
+        handdrawEquationOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handdrawEquationPicture.setImageResource(R.drawable.handdraw_equation_selected);
+                photoEquationPicture.setImageResource(R.drawable.photo_equation);
+                activateStartResolutionButton();
+            }
+        });
+
+        photoEquationOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                photoEquationPicture.setImageResource(R.drawable.photo_equation_selected);
+                handdrawEquationPicture.setImageResource(R.drawable.handdraw_equation);
+                activateStartResolutionButton();
+            }
+        });
+
+        startResolution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(handdrawEquationOption.isChecked()){
+                    Intent intent = new Intent(v.getContext(), EnterEquationHandDrawActivity.class);
+                    startActivity(intent);
+                }else{
+                    Intent intent = new Intent(v.getContext(), CarmeraActivity.class);
+                    //String latex = "\\frac { 3 + 2 ^ { 4 } } { 3 x } = 5";
+                    //ExpressionsManager.setEquationPhoto(latex);
+                    //Intent intent = new Intent(button.getContext(), SolveEquationActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void activateStartResolutionButton(){
+        Button startResolution = (Button) findViewById(R.id.start_resolution_id);
+        startResolution.setEnabled(true);
+        startResolution.setBackgroundResource(R.color.colorGreen);
+        startResolution.setTextColor(Color.WHITE);
     }
 
     @Override
