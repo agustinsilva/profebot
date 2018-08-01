26fd514117438735c1648f45891d041e4f0c142c
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2018-08-01 02:59:43 -0300

    Added more validations of blackboard mapping

--- AppMobile/app/src/main/java/ar/com/profebot/service/ExpressionsManager.java
+++ AppMobile/app/src/main/java/ar/com/profebot/service/ExpressionsManager.java
@@ -214,13 +214,26 @@ public class ExpressionsManager {
                 .replaceAll(",", ".")
                 .replaceAll("\\^\\(\\*\\)", "*") // After replacing [] by (), we must search ^(*)
                 .replaceAll("x", "X")
+
                 .replaceAll("\\.X", "*X")
                 .replaceAll("X\\.", "X*")
                 .replaceAll("\\)X", ")*X")
-                .replaceAll("^\\+", "0+")
-                .replaceAll("^-", "0-")
-                .replaceAll("\\(\\+", "(+")
-                .replaceAll("\\(-", "(-")
+
+                .replaceAll("^\\+\\(", "0+(")
+                .replaceAll("^-\\(", "0-(")
+
+                .replaceAll("^\\+X", "X")
+                .replaceAll("^-X", "0-X")
+
+                .replaceAll("\\(\\+", "(0+")
+                .replaceAll("\\(-", "(0-")
+
+                .replaceAll("=\\+\\(", "=(")
+                .replaceAll("=-\\(", "=0-(")
+
+                .replaceAll("=\\+X", "=X")
+                .replaceAll("=-X", "=0-X")
+
                 .replaceAll("×", "*")
                 .replaceAll("√", "R")
                 .replaceAll("e", "2.718281828459045235360")
