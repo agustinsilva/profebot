d1ddadae9676ca20fb7cf51ca7fb9538aad23d08
Flagohuvelle <leandrogabrielgarcia@gmail.com>
2018-08-09 20:21:39 -0300

    Integracion con modulo inteligente/pending exercises

--- AppMobile/app/src/main/java/ar/com/profebot/activities/ModuloInteligenteTestActivity.java
+++ AppMobile/app/src/main/java/ar/com/profebot/activities/ModuloInteligenteTestActivity.java
@@ -2,11 +2,17 @@ package ar.com.profebot.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
-
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import com.profebot.activities.R;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import ar.com.profebot.Models.ExercisesJson.*;
 import ar.com.profebot.intelligent.module.IAModuleClient;
 import ar.com.profebot.service.ExpressionsManager;
 
@@ -42,5 +48,37 @@ public class ModuloInteligenteTestActivity extends GlobalActivity{
         ((TextView) findViewById(R.id.ecuacion1)).setText(equations[0]);
         ((TextView) findViewById(R.id.ecuacion2)).setText(equations[1]);
         ((TextView) findViewById(R.id.ecuacion3)).setText(equations[2]);
+
+        //saveEquationsFromIAModule(equations);
+    }
+
+    private void saveEquationsFromIAModule(String[] equations) {
+        //Get equations from memory
+        String pendingExercisesJson = PreferenceManager.getDefaultSharedPreferences(this).getString("pendingExercises","");
+        JSONObject equationsJsonToStore = new JSONObject();
+        if (pendingExercisesJson != ""){
+            try{
+                equationsJsonToStore = new JSONObject(pendingExercisesJson);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        try{
+            JSONObject equationsJson = new JSONObject(equations.toString());
+            String equationStr = equationsJson.getString("equations");
+            List<String> equationItems = Arrays.asList(equationStr.split("\\s*;\\s*"));
+            for (Iterator<String> equationString = equationItems.iterator(); equationString.hasNext();) {
+                JSONObject pnObj = new JSONObject();
+                pnObj.put("equation", equationString.next());
+                pnObj.put("difficulty", "Facil");
+                pnObj.put("subject", "Ecuacion simple");
+                equationsJsonToStore.accumulate("pendingExercises", pnObj);
+            }
+            //Save equations in memory
+            PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext()).edit()
+                    .putString("pendingExercises",equationsJsonToStore.toString()).apply();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
