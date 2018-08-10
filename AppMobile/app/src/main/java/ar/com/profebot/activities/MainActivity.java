1574ebb67e3c777dae493c17f70a64ffeab975cc
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2018-08-09 21:16:25 -0300

    Fixeo imports

--- AppMobile/app/src/main/java/ar/com/profebot/activities/MainActivity.java
+++ AppMobile/app/src/main/java/ar/com/profebot/activities/MainActivity.java
@@ -2,6 +2,7 @@ package ar.com.profebot.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
