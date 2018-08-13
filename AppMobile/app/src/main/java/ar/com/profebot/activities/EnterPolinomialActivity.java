f18a0fdb58e4be1b8e3b2bff19eea8afbc0269f6
Flagohuvelle <leandrogabrielgarcia@gmail.com>
2018-08-13 16:26:32 -0300

    small fix in pending layout and first commit for polinomial screen

---
+++ AppMobile/app/src/main/java/ar/com/profebot/activities/EnterPolinomialActivity.java
@@ -0,0 +1,15 @@
+package ar.com.profebot.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.profebot.activities.R;
+
+public class EnterPolinomialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.polinomial_layout);
+    }
+}
