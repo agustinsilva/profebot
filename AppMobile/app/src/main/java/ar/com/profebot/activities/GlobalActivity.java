de3cb165eab1bf09375d748e776b24ab6285c221
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2018-09-17 17:41:22 -0300

    Agrego try-catch global, para evitar crashes de la app

--- AppMobile/app/src/main/java/ar/com/profebot/activities/GlobalActivity.java
+++ AppMobile/app/src/main/java/ar/com/profebot/activities/GlobalActivity.java
@@ -1,5 +1,8 @@
 package ar.com.profebot.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -7,9 +10,44 @@ import android.widget.TextView;
 import com.profebot.activities.R;
 
 public class GlobalActivity extends AppCompatActivity {
+
+    private static Boolean occurredError = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GlobalHelper.setUpMainMenuShortCut((TextView)findViewById(R.id.profebot_id));
+
+        this.setUpGlobalExceptionHandler();
+        this.showErrorPopUpIfOccurredError();
+    }
+
+    private void setUpGlobalExceptionHandler(){
+        GlobalActivity activity = this;
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread paramThread, Throwable paramThrowable) {
+                System.out.println("************************************ ERROR: " + paramThrowable.getMessage() + " ************************************");
+                for(StackTraceElement stackTraceElement : paramThrowable.getStackTrace()){
+                    System.out.println("+++ Clase: " + stackTraceElement.getClassName() + " - Método: " + stackTraceElement.getMethodName() + " - Línea: " + stackTraceElement.getLineNumber());
+                }
+                occurredError = true;
+                startActivity(new Intent(activity, MainActivity.class));
+                System.exit(2);
+            }
+        });
+    }
+
+    // TODO: fixear
+    protected void showErrorPopUpIfOccurredError(){
+        if(occurredError){
+            GlobalActivity activity = this;
+            AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+            alert.setTitle("Tuve un error mientras resolvía el ejercicio :(");
+            alert.setMessage("Lamentablemente ocurrió un error mientras intentaba resolver el ejercicio que ingresaste. ¡Pero no te preocupes! Ahora te llevo al menú principal para que puedas resolver otros ejercicios.");
+            alert.setPositiveButton("OK", null);
+            alert.show();
+            occurredError = false;
+        }
     }
 }
